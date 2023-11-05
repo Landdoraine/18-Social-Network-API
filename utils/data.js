8518fef89ada485ffebcab6c61d27b62cbe0f522
Landdoraine Chen <wending.chen2020@gmail.com>
2023-11-05 00:30:25 -0400

    first commit

---
+++ utils/data.js
@@ -0,0 +1,119 @@
+// data.js
+
+const userData = [
+  {
+    username: "John Doe",
+    email: "john.doe@example.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Alice Smith",
+    email: "alice.smith@example.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Bob Johnson",
+    email: "bob.johnson@example.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Emily Wilson",
+    email: "emily.wilson@example.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "David Brown",
+    email: "david.brown@example.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Grace Miller",
+    email: "grace.miller@example.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Olivia Lee",
+    email: "olivia.lee@example.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Michael Davis",
+    email: "michael.davis@example.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Sophia Taylor",
+    email: "sophia.taylor@example.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Liam Clark",
+    email: "liam.clark@example.com",
+    thoughts: [],
+    friends: [],
+  },
+];
+
+const thoughtData = [
+  {
+    thoughtText: "This is a thought by John Doe",
+    username: "John Doe",
+    reactions: [],
+  },
+  {
+    thoughtText: "Another thought by John Doe",
+    username: "John Doe",
+    reactions: [],
+  },
+  {
+    thoughtText: "A thought by Alice Smith",
+    username: "Alice Smith",
+    reactions: [],
+  },
+  {
+    thoughtText: "Yet another thought by Alice Smith",
+    username: "Alice Smith",
+    reactions: [],
+  },
+  {
+    thoughtText: "Bob Johnson's first thought",
+    username: "Bob Johnson",
+    reactions: [],
+  },
+  {
+    thoughtText: "Emily Wilson's initial thought",
+    username: "Emily Wilson",
+    reactions: [],
+  },
+  {
+    thoughtText: "Thought by David Brown",
+    username: "David Brown",
+    reactions: [],
+  },
+  {
+    thoughtText: "Grace Miller's interesting thought",
+    username: "Grace Miller",
+    reactions: [],
+  },
+  {
+    thoughtText: "Olivia Lee's creative idea",
+    username: "Olivia Lee",
+    reactions: [],
+  },
+  {
+    thoughtText: "A thought by Michael Davis",
+    username: "Michael Davis",
+    reactions: [],
+  },
+];
+
+module.exports = { userData, thoughtData };
